0c0f49e69af8beb96535ff63f6e5fe05ea7f87b7
Hamid Jolany <hamid.jolany@nudgedigital.co.uk>
2021-07-30 17:06:18 +0430

    Unit Test Domain

--- AccountsApi.Tests/V1/Domain/AccountTests.cs
+++ AccountsApi.Tests/V1/Domain/AccountTests.cs
@@ -1,6 +1,7 @@
-using System;
+using System.Linq;
+using System.Reflection.Metadata;
+using AccountsApi.Tests.V1.Helper;
 using AccountsApi.V1.Domain;
-using AccountsApi.V1.Infrastructure;
 using FluentAssertions;
 using Xunit;
 
@@ -9,13 +10,33 @@ namespace AccountsApi.Tests.V1.Domain
     public class AccountTests
     {
         [Fact]
-        public void EntitiesHaveACreatedAt()
+        public void AccountHasPropertiesSet()
         {
-            var entity = new AccountDbEntity();
-            var date = new DateTime(2019, 02, 21);
-            entity.StartDate = date;
 
-            entity.StartDate.Should().BeSameDateAs(date);
+            Account account = Constants.ConstructAccountFromConstants();
+            account.Id.Should().Be(Constants.ID);
+            account.ConsolidatedCharges.Should().ContainSingle();
+            account.ConsolidatedCharges.First().Amount.Should().Be(Constants.AMOUNT);
+            account.ConsolidatedCharges.First().Frequency.Should().Be(Constants.FREQUENCY);
+            account.ConsolidatedCharges.First().Type.Should().Be(Constants.TYPE);
+            account.LastUpdatedDate.Should().Be(Constants.LASTUPDATEDDATE);
+            account.AccountBalance.Should().Be(Constants.ACCOUNTBALANCE);
+            account.AccountStatus.Should().Be(Constants.ACCOUNTSTATUS);
+            account.AccountType.Should().Be(Constants.ACCOUNTTYPE);
+            account.TargetType.Should().Be(Constants.TARGETTYPE);
+            account.TargetId.Should().Be(Constants.TARGETID);
+            account.RentGroupType.Should().Be(Constants.RENTGROUPTYPE);
+            account.AgreementType.Should().Be(Constants.AGREEMENTTYPE);
+            account.CreatedBy.Should().Be(Constants.CREATEDBY);
+            account.LastUpdatedBy.Should().Be(Constants.LASTUPDATEDBY);
+            account.CreatedDate.Should().Be(Constants.CREATEDDATE);
+            account.LastUpdatedDate.Should().Be(Constants.LASTUPDATEDDATE);
+            account.StartDate.Should().Be(Constants.STARTDATE);
+            account.EndDate.Should().Be(Constants.ENDDATE);
+            account.Tenure.TenancyType.Should().Be(Constants.TENURE.TenancyType);
+            account.Tenure.AssetId.Should().Be(Constants.TENURE.AssetId);
+            account.Tenure.FullAddress.Should().Be(Constants.TENURE.FullAddress);
+            account.Tenure.TenancyId.Should().Be(Constants.TENURE.TenancyId);
         }
     }
 }
---
+++ AccountsApi.Tests/V1/Helper/Constants.cs
@@ -0,0 +1,79 @@
+
+using System;
+using System.Collections.Generic;
+using AccountApi.V1.Domain;
+using AccountsApi.V1.Domain;
+
+namespace AccountsApi.Tests.V1.Helper
+{
+    public static class Constants
+    {
+        #region Account
+
+        public static Guid ID { get; }=  Guid.NewGuid();
+        public const TargetType TARGETTYPE = TargetType.Block;
+        public static Guid TARGETID { get; }= Guid.NewGuid();
+        public const AccountType ACCOUNTTYPE = AccountType.Master;
+        public const RentGroupType RENTGROUPTYPE = RentGroupType.Garages;
+        public const string AGREEMENTTYPE = "agreementtype";
+        public const decimal ACCOUNTBALANCE = 0;
+        public const string CREATEDBY = "Admin";
+        public const string LASTUPDATEDBY = "Staff001";
+        public static DateTime CREATEDDATE { get; } =  DateTime.Parse("2020-05-05T15:10:37.471Z");
+        public static DateTime LASTUPDATEDDATE { get; }= DateTime.Parse("2020-05-05T15:10:37.471Z");
+        public static DateTime STARTDATE { get; }= DateTime.Parse("2020-05-05T15:10:37.471Z");
+        public static DateTime ENDDATE { get; } = DateTime.Parse("2020-05-05T15:10:37.471Z");
+        public const AccountStatus ACCOUNTSTATUS = AccountStatus.Active;
+        public static Tenure TENURE { get; }= new Tenure()
+        {
+            AssetId = Guid.NewGuid(),
+            FullAddress = "Hamilton Avenue 12",
+            TenancyType = "Introductory",
+            TenancyId = "123456"
+        };
+        #endregion
+
+        #region ConsolidatedCharge
+        public const string TYPE = "Electricity";
+        public const string FREQUENCY = "Weekly";
+        public const decimal AMOUNT = 125.12M;
+        #endregion
+
+        public static Account ConstructAccountFromConstants()
+        {
+            return new Account
+            {
+                Id = ID,
+                TargetId = TARGETID,
+                TargetType = TARGETTYPE,
+                AccountBalance = ACCOUNTBALANCE,
+                ConsolidatedCharges = new[]
+                {
+                    new ConsolidatedCharge()
+                    {
+                        Amount = AMOUNT,
+                        Frequency = FREQUENCY,
+                        Type = TYPE
+                    }
+                },
+                LastUpdatedDate = LASTUPDATEDDATE,
+                Tenure = new Tenure
+                {
+                    FullAddress = TENURE.FullAddress,
+                    TenancyType = TENURE.TenancyType,
+                    AssetId = TENURE.AssetId,
+                    TenancyId = TENURE.TenancyId
+                },
+                CreatedBy = CREATEDBY,
+                LastUpdatedBy = LASTUPDATEDBY,
+                CreatedDate = CREATEDDATE,
+                AccountStatus = ACCOUNTSTATUS,
+                AccountType = ACCOUNTTYPE,
+                AgreementType = AGREEMENTTYPE,
+                EndDate = ENDDATE,
+                RentGroupType = RENTGROUPTYPE,
+                StartDate = STARTDATE
+            };
+        }
+    }
+}
