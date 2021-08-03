77f46c332f0479326c4d6678a0fe2e547d22d8b3
Hamid Jolany <hamid.jolany@nudgedigital.co.uk>
2021-08-04 01:11:21 +0430

    fixed some merge errors

--- AccountsApi.Tests/V1/Controllers/AccountsApiControllerTests.cs
+++ AccountsApi.Tests/V1/Controllers/AccountsApiControllerTests.cs
@@ -1,14 +1,9 @@
- 
 using AccountsApi.Tests.V1.Helper; 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
-using AccountsApi.Tests.V1.Helper;
-using AccountsApi.V1.Boundary; 
 using AccountsApi.V1.Boundary.Request;
 using AccountsApi.V1.Boundary.Response;
 using AccountsApi.V1.Controllers;
@@ -21,12 +16,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.Data.SqlClient.Server;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace AccountsApi.Tests.V1.Controllers
@@ -43,7 +33,7 @@ namespace AccountsApi.Tests.V1.Controllers
 
         private readonly Fixture _fixture = new Fixture();
 
-        #region GetAllAsync
+
         public AccountsApiControllerTests()
         {
             _getAllUseCase = new Mock<IGetAllUseCase>();
@@ -62,6 +52,8 @@ namespace AccountsApi.Tests.V1.Controllers
             };
         }
 
+
+        #region GetAllAsync
         [Fact]
         public async Task GetAllAsyncFoundReturnResponse()
         {
@@ -155,132 +147,6 @@ namespace AccountsApi.Tests.V1.Controllers
             var accounts = okResult.Value as AccountResponses;
 
             accounts.Should().NotBeNull();
-        }
-
-        [Fact]
-        public async Task GetAllArrears_UseCaseReturnsList_Returns200()
-        {
-            var useCaseResponse = new AccountResponses()
-            {
-                AccountResponseList = new List<AccountModel>()
-                {
-                    new AccountModel()
-                    {
-                        Id = new Guid("b3b91924-1a3d-44b7-b38a-ae4ae5e57b69"),
-                        TargetId = new Guid("2da59b6b-cdcb-46bd-ac61-1c10d1046285"),
-                        StartDate = new DateTime(2021, 7, 1),
-                        CreatedDate = new DateTime(2021, 6, 1),
-                        EndDate = new DateTime(2021, 9, 1),
-                        LastUpdatedDate = new DateTime(2021, 7, 1),
-                        AccountBalance = 100.0M,
-                        AccountStatus = AccountStatus.Active,
-                        AccountType = AccountType.Master,
-                        AgreementType = "string",
-                        CreatedBy = "Admin",
-                        RentGroupType = RentGroupType.Garages,
-                        LastUpdatedBy = "Admin",
-                        TargetType = TargetType.Block
-                    },
-                    new AccountModel()
-                    {
-                        Id = new Guid("17b107e7-b7a1-4c14-a1c9-0630cebdf28e"),
-                        TargetId = new Guid("2da59b6b-cdcb-46bd-ac61-1c10d1046285"),
-                        StartDate = new DateTime(2021, 7, 1),
-                        CreatedDate = new DateTime(2021, 6, 1),
-                        EndDate = new DateTime(2021, 9, 1),
-                        LastUpdatedDate = new DateTime(2021, 7, 1),
-                        AccountBalance = 110.0M,
-                        AccountStatus = AccountStatus.Suspended,
-                        AccountType = AccountType.Master,
-                        AgreementType = "string",
-                        CreatedBy = "Admin",
-                        RentGroupType = RentGroupType.GenFundRents,
-                        LastUpdatedBy = "Admin",
-                        TargetType = TargetType.Core
-                    }
-                }
-            };
-
-            var request = new ArrearRequest()
-            {
-                Type = AccountType.Master,
-                SortBy = "AgreementType",
-                Direction = Direction.Asc
-            };
-
-            _getAllArrearsUseCase.Setup(_ => _.ExecuteAsync(It.IsAny<ArrearRequest>()))
-                .ReturnsAsync(useCaseResponse);
-
-            var result = await _accountApiController.GetArrears(request).ConfigureAwait(false);
-
-            result.Should().NotBeNull();
-
-            var okResult = result as OkObjectResult;
-
-            okResult.Should().NotBeNull();
-
- 
-            okResult.Value.Should().NotBeNull();
-
-            var response = okResult.Value as AccountResponses;
-
-            response.Should().NotBeNull();
-
-            response.AccountResponseList.Should().NotBeNull();
-
-            response.AccountResponseList.Should().HaveCount(2);
-
-            response.AccountResponseList[0].Should().BeEquivalentTo(useCaseResponse.AccountResponseList[0]);
-
-            response.AccountResponseList[1].Should().BeEquivalentTo(useCaseResponse.AccountResponseList[1]);
-
-        }
-
-        [Fact]
-        public async Task GetAllArrears_NullRequestModel_Returns400()
-        {
-            ArrearRequest request = null;
-
-            var result = await _accountApiController.GetArrears(request).ConfigureAwait(false);
-
-            result.Should().NotBeNull();
-
-            var badRequest = result as BadRequestObjectResult;
-
-            badRequest.Should().NotBeNull();
-
-            badRequest.Value.Should().NotBeNull();
-
-            var baseErrorResponse = badRequest.Value as BaseErrorResponse;
-
-            baseErrorResponse.Should().NotBeNull();
-
-            baseErrorResponse.StatusCode.Should().Be((int) HttpStatusCode.BadRequest);
-
-            baseErrorResponse.Message.Should().BeEquivalentTo("ArrearRequest can't be null");
-
-            baseErrorResponse.Details.Should().BeEquivalentTo(string.Empty);
-        }
-
-        [Fact]
-        public async Task GetAllArrears_UseCaseThrowException_Returns500()
-        {
-            var request = new ArrearRequest();
-
-            _getAllArrearsUseCase.Setup(_ => _.ExecuteAsync(It.IsAny<ArrearRequest>()))
-                .ThrowsAsync(new Exception("Test exception"));
-
-            try
-            {
-                var result = await _accountApiController.GetArrears(new ArrearRequest()).ConfigureAwait(false);
-                AssertExtensions.Fail();
-            }
-            catch (Exception ex)
-            {
-                ex.GetType().Should().Be(typeof(Exception));
-                ex.Message.Should().Be("Test exception");
-            }
- 
             accounts.AccountResponseList.Should().HaveCount(2);
 
             accounts.AccountResponseList[0].Id.Should().Be(Guid.Parse("82aa6932-e98d-41a1-a4d4-2b44135554f8"));
