cb95ff55943259211b26ff8c8876e721a9dd1dca
Hamid Jolany <hamid.jolany@nudgedigital.co.uk>
2021-08-24 17:14:33 +0430

    Some unnecessary methods definition omitted New methods and use cases defined

--- AccountsApi/V1/UseCase/Interfaces/IAddUseCase.cs
+++ AccountsApi/V1/UseCase/Interfaces/IAddUseCase.cs
@@ -1,15 +1,11 @@
+using AccountsApi.V1.Boundary.Request;
 using AccountsApi.V1.Boundary.Response;
-using AccountsApi.V1.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace AccountsApi.V1.UseCase.Interfaces
 {
     public interface IAddUseCase
     {
-        public AccountResponseObject Execute(Account account);
-        public Task<AccountResponseObject> ExecuteAsync(Account account);
+        public Task<AccountModel> ExecuteAsync(AccountRequest account);
     }
 }
--- AccountsApi/V1/UseCase/Interfaces/IGetAllUseCase.cs
+++ AccountsApi/V1/UseCase/Interfaces/IGetAllUseCase.cs
@@ -1,4 +1,5 @@
 using AccountsApi.V1.Boundary.Response;
+using AccountsApi.V1.Domain;
 using System;
 using System.Threading.Tasks;
 
@@ -6,6 +7,6 @@ namespace AccountsApi.V1.UseCase.Interfaces
 {
     public interface IGetAllUseCase
     {
-        public Task<AccountResponseObjectList> ExecuteAsync(Guid targetId);
+        public Task<AccountResponses> ExecuteAsync(Guid targetId, AccountType accountType);
     }
 }
