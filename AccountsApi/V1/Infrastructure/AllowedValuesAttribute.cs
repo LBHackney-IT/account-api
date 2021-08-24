27d213d089a449514758df144a9f6771e7df2903
Hamid Jolany <hamid.jolany@nudgedigital.co.uk>
2021-08-24 16:59:17 +0430

    Define allowed attribute as an annotation

---
+++ AccountsApi/V1/Infrastructure/AllowedValuesAttribute.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace AccountsApi.V1.Infrastructure
+{
+    public class AllowedValuesAttribute : ValidationAttribute
+    {
+        private readonly Type _type;
+
+        public AllowedValuesAttribute(Type enumType)
+        {
+            _type = enumType;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var valueType = value.GetType();
+
+            if (!valueType.IsEnum || !Enum.IsDefined(_type, value))
+            {
+                var values = Enum.GetNames(_type);
+                return new ValidationResult($"{validationContext.MemberName} field should be a type of {_type.Name} enum. Values: {string.Join(", ", values.Select(a => a))}");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
