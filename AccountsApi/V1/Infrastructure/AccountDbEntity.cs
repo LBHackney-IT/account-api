66580b144583992ee23d8ee15f12b3d2f3504880
Hamid Jolany <hamid.jolany@nudgedigital.co.uk>
2021-08-24 17:22:25 +0430

    Infrastructures changes

--- AccountsApi/V1/Infrastructure/AccountDbEntity.cs
+++ AccountsApi/V1/Infrastructure/AccountDbEntity.cs
@@ -1,36 +1,66 @@
 using AccountsApi.V1.Domain;
 using Amazon.DynamoDBv2.DataModel;
 using System;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 
 namespace AccountsApi.V1.Infrastructure
 {
-    [Table("accounts")]
     [DynamoDBTable("accounts", LowerCamelCaseProperties = true)]
     public class AccountDbEntity
     {
         [DynamoDBHashKey]
         [DynamoDBProperty(AttributeName = "id")]
         public Guid Id { get; set; }
-        [DynamoDBProperty(AttributeName = "target_id")]
-        public Guid TargetId { get; set; }
-        [DynamoDBProperty(AttributeName = "target_type", Converter = typeof(DynamoDbEnumConverter<TargetType>))]
-        public TargetType TargetType { get; set; }
-        [DynamoDBProperty(AttributeName = "account_balance")]
-        public decimal AccountBalance { get; set; }
+
+        [DynamoDBProperty(AttributeName="parent_account")]
+        public Guid ParentAccount { get; set; }
+
         [DynamoDBProperty(AttributeName = "payment_reference")]
         public string PaymentReference { get; set; }
-        [DynamoDBProperty(AttributeName = "last_updated", Converter = typeof(DynamoDbDateTimeConverter))]
-        public DateTime LastUpdated { get; set; }
+
+        [DynamoDBProperty(AttributeName = "target_type", Converter = typeof(DynamoDbEnumConverter<TargetType>))]
+        public TargetType TargetType { get; set; }
+
+        [DynamoDBProperty(AttributeName = "target_id")]
+        public Guid TargetId { get; set; }
+
+        [DynamoDBProperty(AttributeName = "account_type", Converter = typeof(DynamoDbEnumConverter<AccountType>))]
+        public AccountType AccountType { get; set; }
+
+        [DynamoDBProperty(AttributeName = "rent_group_type", Converter = typeof(DynamoDbEnumConverter<RentGroupType>))]
+        public RentGroupType RentGroupType { get; set; }
+
+        [DynamoDBProperty(AttributeName = "agreement_type")]
+        public string AgreementType { get; set; }
+
+        [DynamoDBProperty(AttributeName = "account_balance")]
+        public decimal AccountBalance { get; set; }
+
+        [DynamoDBProperty(AttributeName = "created_by")]
+        public string CreatedBy { get; set; }
+
+        [DynamoDBProperty(AttributeName = "last_updated_by")]
+        public string LastUpdatedBy { get; set; }
+
+        [DynamoDBProperty(AttributeName = "created_date", Converter = typeof(DynamoDbDateTimeConverter))]
+        public DateTime CreatedDate { get; set; }
+
+        [DynamoDBProperty(AttributeName = "last_updated_date", Converter = typeof(DynamoDbDateTimeConverter))]
+        public DateTime LastUpdatedDate { get; set; }
+
         [DynamoDBProperty(AttributeName = "start_date", Converter = typeof(DynamoDbDateTimeConverter))]
         public DateTime StartDate { get; set; }
+
         [DynamoDBProperty(AttributeName = "end_date", Converter = typeof(DynamoDbDateTimeConverter))]
         public DateTime EndDate { get; set; }
+
         [DynamoDBProperty(AttributeName = "account_status", Converter = typeof(DynamoDbEnumConverter<AccountStatus>))]
         public AccountStatus AccountStatus { get; set; }
-        [DynamoDBProperty(AttributeName = "total_charged")]
-        public decimal TotalCharged { get; set; }
-        [DynamoDBProperty(AttributeName = "total_paid")]
-        public decimal TotalPaid { get; set; }
+
+        [DynamoDBProperty(AttributeName = "consolidated_charges", Converter = typeof(DynamoDbObjectListConverter<ConsolidatedCharges>))]
+        public IEnumerable<ConsolidatedCharges> ConsolidatedCharges { get; set; }
+
+        [DynamoDBProperty(AttributeName = "tenure", Converter = typeof(DynamoDbObjectConverter<Tenure>))]
+        public Tenure Tenure { get; set; }
     }
 }
