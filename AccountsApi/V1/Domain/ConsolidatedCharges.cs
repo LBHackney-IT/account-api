92947be7fdde51c3b2aff56a6e5d50f29f378cbb
Hamid Jolany <hamid.jolany@nudgedigital.co.uk>
2021-08-24 17:06:40 +0430

    Entities Amendment

--- AccountsApi/V1/Domain/ConsolidatedCharges.cs
+++ AccountsApi/V1/Domain/ConsolidatedCharges.cs
@@ -1,14 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
 
 namespace AccountsApi.V1.Domain
 {
     public class ConsolidatedCharges
     {
+        /// <example>
+        ///     Rent
+        /// </example>
+        [NotNull]
         public string Type { get; set; }
+
+        /// <example>
+        ///     Weekly
+        /// </example>
+        [NotNull]
         public string Frequency { get; set; }
+
+        /// <example>
+        ///     101.20
+        /// </example>
+        [NotNull]
         public decimal Amount { get; set; }
     }
 }
