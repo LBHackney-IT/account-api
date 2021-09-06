0dde5668a8d1ec7d7f6043273922e458582a8f49
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-09-06 21:01:53 +0430

    validationContext.MemberNam empty, null or whitespace checking

--- AccountsApi/V1/Infrastructure/AllowedValuesAttribute.cs
+++ AccountsApi/V1/Infrastructure/AllowedValuesAttribute.cs
@@ -15,19 +15,25 @@ namespace AccountsApi.V1.Infrastructure
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
+            string memberName = "";
+
+            if (!string.IsNullOrWhiteSpace(validationContext.MemberName) ||
+                !string.IsNullOrEmpty(validationContext.MemberName))
+                memberName = validationContext.MemberName;
+
             if (value == null)
-                return new ValidationResult($"{validationContext.MemberName} field should not be null");
+                return new ValidationResult($"{memberName} field should not be null");
 
             var valueType = value.GetType();
 
             if (!valueType.IsEnum)
             {
-                return new ValidationResult($"{validationContext.MemberName} field should be a type of enum.");
+                return new ValidationResult($"{memberName} field should be a type of enum.");
             }
             else if (!Enum.IsDefined(_type, value))
             {
                 var values = Enum.GetNames(_type);
-                return new ValidationResult($"{validationContext.MemberName} field should be a type of {_type.Name} enum. Values: {string.Join(", ", values.Select(a => a))}");
+                return new ValidationResult($"{memberName} field should be a type of {_type.Name} enum. Values: {string.Join(", ", values.Select(a => a))}");
             }
 
             return ValidationResult.Success;
