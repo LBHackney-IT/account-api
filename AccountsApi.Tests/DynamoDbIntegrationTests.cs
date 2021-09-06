9bd8200400ef202c3f7ef0d8619c464d7ab2bddb
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-09-07 01:47:49 +0430

    Boundry Unit Testing

--- AccountsApi.Tests/DynamoDbIntegrationTests.cs
+++ AccountsApi.Tests/DynamoDbIntegrationTests.cs
@@ -1,65 +1,111 @@
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
-using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 
 namespace AccountsApi.Tests
 {
-    public class DynamoDbIntegrationTests<TStartup> where TStartup : class
+    public class DynamoDbIntegrationTests<TStartup> : IDisposable where TStartup : class
     {
         protected HttpClient Client { get; private set; }
-        private DynamoDbMockWebApplicationFactory<TStartup> _factory;
+        private readonly DynamoDbMockWebApplicationFactory<TStartup> _factory;
         protected IDynamoDBContext DynamoDbContext => _factory?.DynamoDbContext;
-        protected List<Action> CleanupActions { get; set; }
+        protected List<Action> CleanupActions { get; }
 
         private readonly List<TableDef> _tables = new List<TableDef>
         {
-            // TODO: Populate the list of table(s) and their key property details here, for example:
-            //new TableDef { Name = "example_table", KeyName = "id", KeyType = ScalarAttributeType.N }
+            new TableDef()
+            {
+                TableName = "accounts",
+                PartitionKey = new AttributeDef()
+                {
+                    KeyName = "id",
+                    KeyType = KeyType.HASH,
+                    KeyScalarType = ScalarAttributeType.S
+                },
+                Indices = new List<GlobalIndexDef>{
+                    new GlobalIndexDef()
+                    {
+                        KeyName = "target_id",
+                        KeyType = KeyType.HASH,
+                        KeyScalarType = ScalarAttributeType.S,
+                        IndexName = "target_id_dx",
+                        ProjectionType = "ALL"
+                    },
+                    new GlobalIndexDef()
+                    {
+                        KeyName = "account_type",
+                        KeyType = KeyType.HASH,
+                        KeyScalarType = ScalarAttributeType.S,
+                        IndexName = "account_type_dx",
+                        ProjectionType = "ALL"
+                    }
+                }
+            },
         };
 
         private static void EnsureEnvVarConfigured(string name, string defaultValue)
         {
             if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable(name)))
+            {
                 Environment.SetEnvironmentVariable(name, defaultValue);
+            }
         }
 
-        [OneTimeSetUp]
-        public void OneTimeSetUp()
+        public DynamoDbIntegrationTests()
         {
             EnsureEnvVarConfigured("DynamoDb_LocalMode", "true");
             EnsureEnvVarConfigured("DynamoDb_LocalServiceUrl", "http://localhost:8000");
+            EnsureEnvVarConfigured("DynamoDb_LocalSecretKey", "vymxp");
+            EnsureEnvVarConfigured("DynamoDb_LocalAccessKey", "2cl9i");
             _factory = new DynamoDbMockWebApplicationFactory<TStartup>(_tables);
-        }
 
-        [OneTimeTearDown]
-        public void OneTimeTearDown()
-        {
-            _factory.Dispose();
-        }
-
-        [SetUp]
-        public void BaseSetup()
-        {
             Client = _factory.CreateClient();
             CleanupActions = new List<Action>();
         }
 
-        [TearDown]
-        public void BaseTearDown()
+        public void Dispose()
         {
-            foreach (var act in CleanupActions)
-                act();
-            Client.Dispose();
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private bool _disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && !_disposed)
+            {
+                foreach (var act in CleanupActions)
+                {
+                    act();
+                }
+                Client.Dispose();
+
+                if (null != _factory)
+                    _factory.Dispose();
+                _disposed = true;
+            }
         }
     }
 
     public class TableDef
     {
-        public string Name { get; set; }
+        public string TableName { get; set; }
+        public AttributeDef PartitionKey { get; set; }
+        public List<GlobalIndexDef> Indices { get; set; }
+    }
+
+    public class AttributeDef
+    {
         public string KeyName { get; set; }
-        public ScalarAttributeType KeyType { get; set; }
+        public ScalarAttributeType KeyScalarType { get; set; }
+        public KeyType KeyType { get; set; }
+    }
+
+    public class GlobalIndexDef : AttributeDef
+    {
+        public string IndexName { get; set; }
+        public string ProjectionType { get; set; }
     }
 }
---
+++ AccountsApi.Tests/V1/Boundary/Request/AccountRequestTests.cs
@@ -0,0 +1,42 @@
+using AccountsApi.V1.Boundary.Request;
+using AccountsApi.V1.Domain;
+using AutoFixture;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace AccountsApi.Tests.V1.Boundary.Request
+{
+    public class AccountRequestTests
+    {
+
+        private readonly Fixture _fixture;
+        public AccountRequestTests()
+        {
+            _fixture = new Fixture();
+        }
+        [Fact]
+        public void AccountRequestHasPropertiesSet()
+        {
+            var request = typeof(AccountRequest);
+            request.GetProperties().Length.Should().Be(14);
+
+            AccountRequest account = _fixture.Create<AccountRequest>();
+
+            Assert.IsType<AccountStatus>(account.AccountStatus);
+            Assert.IsType<AccountType>(account.AccountType);
+            Assert.IsType<string>(account.AgreementType);
+            Assert.IsType<string>(account.CreatedBy);
+            Assert.IsType<DateTime>(account.CreatedDate);
+            Assert.IsType<DateTime>(account.EndDate);
+            Assert.IsType<string>(account.LastUpdatedBy);
+            Assert.IsType<DateTime>(account.LastUpdatedDate);
+            Assert.IsType<Guid>(account.ParentAccount);
+            Assert.IsType<string>(account.PaymentReference);
+            Assert.IsType<RentGroupType>(account.RentGroupType);
+            Assert.IsType<DateTime>(account.StartDate);
+            Assert.IsType<Guid>(account.TargetId);
+            Assert.IsType<TargetType>(account.TargetType);
+        }
+    }
+}
