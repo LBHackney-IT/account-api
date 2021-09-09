742f8cc8af63d4a0e64afb6ab6bf1777ad8cc8be
LBHSPreston <Selwyn.Preston@hackney.gov.uk>
2021-09-09 15:42:39 +0100

    create and update events constants

---
+++ AccountsApi/V1/Infrastructure/CreateEventConstants.cs
@@ -0,0 +1,11 @@
+namespace AccountsApi.V1.Infrastructure
+{
+    public static class CreateEventConstants
+    {
+        // JWT TOKEN
+        public const string V1VERSION = "v1";
+        public const string EVENTTYPE = "AccountCreatedEvent";
+        public const string SOURCEDOMAIN = "Accounts";
+        public const string SOURCESYSTEM = "AccountsAPI";
+    }
+}
---
+++ AccountsApi/V1/Infrastructure/UpdateEventConstants.cs
@@ -0,0 +1,11 @@
+namespace AccountsApi.V1.Infrastructure
+{
+    public static class UpdateEventConstants
+    {
+        // JWT TOKEN
+        public const string V1VERSION = "v1";
+        public const string EVENTTYPE = "PersonUpdatedEvent";
+        public const string SOURCEDOMAIN = "Person";
+        public const string SOURCESYSTEM = "PersonAPI";
+    }
+}
