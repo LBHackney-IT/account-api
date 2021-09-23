331554e0dfe11830982712e660b4001e8566e126
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-09-23 17:09:33 +0330

    Indexes were defined as attribute in terraform

--- terraform/staging/dynamodb.tf
+++ terraform/staging/dynamodb.tf
@@ -10,6 +10,16 @@ resource "aws_dynamodb_table" "accountsapi_dynamodb_table" {
         type              = "S"
     }
 
+    attribute {
+        name              = "account_type"
+        type              = "S"
+    }
+	
+    attribute {
+        name              = "target_id"
+        type              = "S"
+    }
+
     tags = {
         Name              = "accounts-api-${var.environment_name}"
         Environment       = var.environment_name
