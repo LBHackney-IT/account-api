c0c2276fedf31616836af33279e18d72012e8c47
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-09-30 14:22:56 +0330

    NonEmptyGuid instead of required

--- AccountsApi/V1/Domain/Person.cs
+++ AccountsApi/V1/Domain/Person.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+using AccountsApi.V1.Infrastructure;
 
 namespace AccountsApi.V1.Domain
 {
     public class Person
     {
-        [Required]
+        [NonEmptyGuid]
         public Guid Id { get; set; }
         [Required]
         public string FullName { get; set; }
