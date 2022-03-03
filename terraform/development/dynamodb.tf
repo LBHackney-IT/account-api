b4007f6427b41b756b446ebef3c11c23cefb8737
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2022-03-03 19:31:21 +0330

    dynamodb configuration about auto scaling mode

--- terraform/development/dynamodb.tf
+++ terraform/development/dynamodb.tf
@@ -48,31 +48,6 @@ resource "aws_dynamodb_table" "accountsapi_dynamodb_table" {
     }
 }
 
-
-resource "aws_appautoscaling_target" "dynamodb_table_read_target" {
-  max_capacity       = 600
-  min_capacity       = 100
-  resource_id        = "table/${aws_dynamodb_table.accountsapi_dynamodb_table.name}"
-  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
-  service_namespace  = "dynamodb"
-}
-
-resource "aws_appautoscaling_policy" "dynamodb_table_read_policy" {
-  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_read_target.resource_id}"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.dynamodb_table_read_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.dynamodb_table_read_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.dynamodb_table_read_target.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "DynamoDBReadCapacityUtilization"
-    }
-
-    target_value = 80
-  }
-}
-
 resource "aws_appautoscaling_target" "dynamodb_table_write_target" {
   max_capacity       = 600
   min_capacity       = 100
