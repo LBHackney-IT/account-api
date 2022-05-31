925afd277685cc1ad43c1547cfc222bb9c6ad83b
Faisal Gazi <faisal.gazi@hackney.gov.uk>
2022-05-31 20:16:51 +0100

    removed duplicate resource
    1) Removed auto-scaling policy and target resources as terraform was complaining about as duplicates
    2) Correction to resource_id syntax to the db index

--- terraform/staging/dynamodb.tf
+++ terraform/staging/dynamodb.tf
@@ -73,56 +73,10 @@ resource "aws_appautoscaling_policy" "dynamodb_table_write_policy" {
   }
 }
 
-
-
-
 resource "aws_appautoscaling_target" "dynamodb_index_write_target" {
   max_capacity       = 600
   min_capacity       = 100
-  resource_id        = "table/${aws_dynamodb_table.accountsapi_dynamodb_table.name/index/account_type_dx}"
+  resource_id        = "table/${aws_dynamodb_table.accountsapi_dynamodb_table.name}/index/account_type_dx"
   scalable_dimension = "dynamodb:table:WriteCapacityUnits"
   service_namespace  = "dynamodb"
-}
-
-resource "aws_appautoscaling_policy" "dynamodb_table_write_policy" {
-  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_write_target.resource_id}"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.dynamodb_table_write_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.dynamodb_table_write_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.dynamodb_table_write_target.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
-    }
-
-    target_value = 70
-  }
-}
-
-
-
-
-resource "aws_appautoscaling_target" "dynamodb_index_write_target" {
-  max_capacity       = 600
-  min_capacity       = 100
-  resource_id        = "table/${aws_dynamodb_table.accountsapi_dynamodb_table.name/index/target_id_dx}"
-  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
-  service_namespace  = "dynamodb"
-}
-
-resource "aws_appautoscaling_policy" "dynamodb_table_write_policy" {
-  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_write_target.resource_id}"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.dynamodb_table_write_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.dynamodb_table_write_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.dynamodb_table_write_target.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
-    }
-
-    target_value = 70
-  }
 }
