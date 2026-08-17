632a39d7d167658756c4a62e37783f85a4cc5939
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2026-08-17 11:16:02 +0100

    Replace the deprecated data.aws_region.current `name` attribute with `region`.

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -25,7 +25,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 locals {
-    parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
+    parameter_store = "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter"
 }
 
 terraform {
